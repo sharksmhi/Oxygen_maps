f68cc74454169bc37dc1e319b38685f03f56ee5e
Viktorsson Lena <a002087@ad.smhi.se>
2023-05-11 18:17:10 +0200

    anox and hypox area maps

--- python_code/read_nc_file.py
+++ python_code/read_nc_file.py
@@ -1,36 +1,95 @@
 import xarray as xr
 import numpy as np
 
-
 ### open netcdf file ###
-ds = xr.open_dataset("C:/Work/DIVAnd/Oxygen_maps/resultat/nc/O2/Oxygen_Autumn_2021_gebco_30sec_4.nc")
-#print(df)
-### extract values that are within our limits, save to a new variable and nc-file. ####
+ds = xr.open_dataset("resultat/nc/O2/Oxygen_Autumn_1960_gebco_30sec_8.nc")
 
+### extract values that are within our limits, save to a new variable and nc-file. ####
+hypox = 2
+anox = 0.1
 var_name = "Oxygen"
 ds["ANOX"]=xr.where((ds[var_name]<=0.5),ds[var_name]/ds[var_name]*1,ds[var_name]*np.nan,keep_attrs=True)
-ds["HYPOX"]=xr.where((ds[var_name]<=2),ds[var_name]/ds[var_name]*1,ds[var_name]*np.nan,keep_attrs=True)
-#ds["HYPOX"]=xr.where(((ds["HYPOX"]>=2)),ds["HYPOX"]*np.nan,ds["HYPOX"],keep_attrs=True)
+ds["HYPOX"]=xr.where((ds[var_name]<=hypox),ds[var_name]/ds[var_name]*1,ds[var_name]*np.nan,keep_attrs=True)
 
-###To do: Stack results from all depts to a map ###
+threshold = hypox  # Define the threshold value
+ds["hypox_depth"]=xr.where((ds[var_name]<=hypox),ds['depth'],ds[var_name]*np.nan,keep_attrs=True)
+# Find the minimum threshold depth at each (time, lon, lat) coordinate
+ds['min_hypox_depth']=ds['hypox_depth'].min(dim='depth', skipna=True)
+threshold = anox  # Define the threshold value
+ds["anox_depth"]=xr.where((ds[var_name]<=anox),ds['depth'],ds[var_name]*np.nan,keep_attrs=True)
+# Find the minimum threshold depth at each (time, lon, lat) coordinate
+ds['min_anox_depth']=ds['anox_depth'].min(dim='depth', skipna=True)
 
-print(ds[var_name].depth[0])
-#xr.Dataset.stack(ds["HYPOX"].depth)
-#ds["HYPOX_depth"] = xr.where((ds[var_name] < 20), depth, ds[var_name],
- #                                keep_attrs=True)
-#for depth in ds["HYPOX_depth"].depth:
+# Print the updated dataset
+print(ds)
+ds.to_netcdf('resultat/nc/modified_test.nc') # rewrite to netcdf
 
-#ds["HYPOX_depth"]=xr.where((ds[var_name.depth[0]]<20),ds[var_name]/ds[var_name]*1,ds[var_name],keep_attrs=True)
-#ds["HYPOX_depth"]=xr.where(((ds["HYPOX_depth"]<50) & (ds["HYPOX_depth"]>=20)),ds["HYPOX_depth"]/ds["HYPOX_depth"]*2,ds["HYPOX_depth"],keep_attrs=True)
-#ds["HYPOX_depth"]=xr.where(((ds["HYPOX_depth"]>=50)),ds["HYPOX_depth"]*np.nan,ds["HYPOX_depth"],keep_attrs=True)
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-#ds["HYPOX"]=xr.where((ds[var_name]<50),ds[var_name]/ds[var_name]*2,ds[var_name]*np.nan,keep_attrs=True)
-#xr.where((ds[var_name]<0.5),ds[var_name]/ds[var_name]*0,ds[var_name]*np.nan,keep_attrs=True)
-ds.to_netcdf('modified_test.nc') # rewrite to netcdf
+# Plot the data on a map
+# Create a 2x2 grid of subplots
+fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+# set common max, min limits for all plots
+vmin = 60
+vmax = 200
+# Select a specific time index and depth level
+time_index = 0  # Replace with the desired time index
+# Extract year and month from the time value
+print(repr(ds["time"][time_index].values))
+time_value = ds['time'][time_index].values.astype('datetime64[M]').item()
+year_month = datetime.strftime(time_value, '%Y-%m')
+# 1111111 Plot the data on the 1st subplot
+data = ds['min_hypox_depth'].sel(time=ds['time'][time_index])
+data.plot(ax=axs[0, 0], x='lon', y='lat', cmap='viridis', vmin=vmin, vmax=vmax)
+# Add labels to the 1st subplot
+axs[0, 0].set_title(f'minimum depths where oxygen <={hypox} ml/l')
+axs[0, 0].set_xlabel('Longitude')
+axs[0, 0].set_ylabel('Latitude')
 
+# 2222222 Plot the data on the 2nd subplot
+data = ds['min_anox_depth'].sel(time=ds['time'][time_index])
+data.plot(ax=axs[0, 1], x='lon', y='lat', cmap='viridis', vmin=vmin, vmax=vmax)
 
-#O2_L2 = df['Oxygen_L2']
-#print(df.Oxygen_L2)
-#df.Oxygen_L2.sel()
-#O2_L2_flat = df['Oxygen_L2'].values.flatten() # 1-d datadf
+# Add labels to the 2nd subplot
+axs[0, 1].set_title(f'minimum depths where oxygen <={anox} ml/l')
+axs[0, 1].set_xlabel('Longitude')
+axs[0, 1].set_ylabel('Latitude')
+# Set a common title for each row of subplots
+fig.text(0.5, 0.94, f'{year_month} (Time index: {time_index})', ha='center')
 
+# Select the last time index
+last_time_index = -1
+time_value = ds['time'][last_time_index].values.astype('datetime64[M]').item()
+year_month = datetime.strftime(time_value, '%Y-%m')
+
+# Plot the data on the 3rd subplot
+data = ds['min_hypox_depth'].sel(time=ds['time'][last_time_index])
+data.plot(ax=axs[1, 0], x='lon', y='lat', cmap='viridis', vmin=vmin, vmax=vmax)
+axs[1, 0].set_title(f'minimum depths where oxygen <= {hypox} ml/l')
+axs[1, 0].set_xlabel('Longitude')
+axs[1, 0].set_ylabel('Latitude')
+
+# Plot the data on the 4th subplot
+data = ds['min_anox_depth'].sel(time=ds['time'][last_time_index])
+data.plot(ax=axs[1, 1], x='lon', y='lat', cmap='viridis', vmin=vmin, vmax=vmax)
+axs[1, 1].set_title(f'minimum depths where oxygen <= {anox} ml/l')
+axs[1, 1].set_xlabel('Longitude')
+axs[1, 1].set_ylabel('Latitude')
+
+# Set a common title for each row of subplots
+axs[0, 0].get_shared_x_axes().join(axs[0, 0], axs[0, 1])
+axs[0, 0].get_shared_y_axes().join(axs[0, 0], axs[0, 1])
+
+# Set a common title for each row of subplots
+fig.text(0.5, 0.47, f'{year_month} (Time index: {last_time_index})', ha='center')
+
+# Adjust the spacing between subplots
+fig.tight_layout(rect=[0, 0, 1, 0.95])
+
+# Add title and labels
+# Set the title for the whole figure
+fig.suptitle(f'maps of hypoxia and anoxia')
+
+# Save the plot
+plt.savefig(f'resultat/figures/_hypox.png')
