2f87e48ed38cacd2f9c0379d37fb445a39c79eef
Lena <a002087@ad.smhi.se>
2024-04-29 21:25:26 +0200

    fix barchart

--- python_code/plot_area_timeseries.py
+++ python_code/plot_area_timeseries.py
@@ -5,12 +5,13 @@ import matplotlib.ticker as ticker
 from datetime import datetime
 import pandas as pd
 
-start_year = 2021
-end_year = 2021
+start_year = 1960
+end_year = 2022
 
 location = "//winfs-proj/proj/havgem/DIVA/syrekartor/" # or other location, like havgem path
 df = pd.read_csv(f'{location}resultat/area_data_{start_year}_{end_year}.txt', sep = '\t')
 df_matlab = pd.read_csv(f'{location}resultat/area_volume_data_from_matlab.txt', sep = '\t')
+df_matlab.rename(columns={"Anoxic_area_km2": "Area_0_km2", "Hypoxic_area_km2": "Area_90_km2"}, inplace=True)
 
 mpl.rcParams['hatch.linewidth'] = 0.5 # sets the linewidth globally of all hatch lines
 plt.rcParams['axes.axisbelow'] = True # makes the grid stay behind the bars
@@ -34,8 +35,8 @@ ax[0].xaxis.set_minor_locator(ticker.MultipleLocator(1))
 for season, subset in df.groupby('season'):
     subplot_row = subplot_order[season]
     # plot bars
-    ax[subplot_row].bar(x=subset['year'], height=subset[f"Area_180_km2"], width=1, color='b', edgecolor='black',linewidth=0.5, label='180')
-    ax[subplot_row].bar(x=subset['year'], height=subset['Relerr_area_180_km2'],bottom=subset['Area_180_km2'] - subset['Relerr_area_180_km2'], width=1, color='b', hatch='/////',edgecolor='black', linewidth=0.5, label="180 Relerror >0.5")
+    ax[subplot_row].bar(x=subset['year'], height=subset[f"Area_180_km2"], width=1, color='b', edgecolor='black',linewidth=0.5, label='180 umol/l')
+    ax[subplot_row].bar(x=subset['year'], height=subset['Relerr_area_180_km2'],bottom=subset['Area_180_km2'] - subset['Relerr_area_180_km2'], width=1, color='b', hatch='/////',edgecolor='black', linewidth=0.5, label="180 umol/l Relerror >0.5")
     # plot hypoxic bars
     ax[subplot_row].bar(x=subset['year'], height=subset[f"Area_90_km2"], width = 1, color = 'r', edgecolor = 'black', linewidth = 0.5, label = 'Hypoxic')
     ax[subplot_row].bar(x=subset['year'], height=subset['Relerr_area_90_km2'], bottom = subset['Area_90_km2']-subset['Relerr_area_90_km2'], width = 1, color = 'r', hatch='/////', edgecolor = 'black', linewidth = 0.5, label="Hypoxic Relerror >0.5")
@@ -49,7 +50,7 @@ for season, subset in df.groupby('season'):
     ax[subplot_row].ticklabel_format(style='scientific', axis = 'y')
 
 # turn on legend and set position
-ax[0].legend(loc=(0.8,1.1))
+ax[0].legend(ncols=3, loc=(0.1,1.1))
 
 # save figure
 fig.savefig(f'{location}resultat/figures/{df.year.min()}-{df.year.max()}_barchart.png', dpi = 300)
@@ -68,15 +69,15 @@ ax[0].xaxis.set_minor_locator(ticker.MultipleLocator(1))
 
 df_merged = pd.merge(df.loc[df['season']=='Autumn'], df_matlab, on='year', how='outer', suffixes=['_DIVAnd', '_matlab'], sort=True)
 # plot hypoxic bars
-df_merged.plot.bar(ax=ax[0], x='year', y='Hypoxic_area_km2_matlab', width = 1, color = 'red', label='matlab')
-df_merged.plot.bar(ax=ax[0], x='year', y='Hypoxic_area_km2_DIVAnd', width = 1, color = 'none', hatch='/////', edgecolor = 'black', linewidth = 0.5, label='DIVAnd')
+df_merged.plot.bar(ax=ax[0], x='year', y='Area_90_km2_matlab', width = 1, color = 'red', label='matlab')
+df_merged.plot.bar(ax=ax[0], x='year', y='Area_90_km2_DIVAnd', width = 1, color = 'none', hatch='/////', edgecolor = 'black', linewidth = 0.5, label='DIVAnd')
 
 ax[0].set_title('Hypoxia')
 ax[0].set_ylabel('area km$^2$') 
 ax[0].grid(which = 'both')
 # plot anoxic bars
-df_merged.plot.bar(ax=ax[1], x='year', y='Anoxic_area_km2_matlab', width = 1, color = 'red', label='matlab')
-df_merged.plot.bar(ax=ax[1], x='year', y='Anoxic_area_km2_DIVAnd', width = 1, color = 'none', hatch='/////', edgecolor = 'black', linewidth = 0.5, label='DIVAnd')
+df_merged.plot.bar(ax=ax[1], x='year', y='Area_0_km2_matlab', width = 1, color = 'red', label='matlab')
+df_merged.plot.bar(ax=ax[1], x='year', y='Area_0_km2_DIVAnd', width = 1, color = 'none', hatch='/////', edgecolor = 'black', linewidth = 0.5, label='DIVAnd')
 #df_merged.plot.bar(ax=ax[1], x='year', y='Anoxic_area_km2_DIVAnd', width = 1, color = 'r', label='DIVAnd')
 #df_merged.plot.bar(ax=ax[1], x='year', y='Anoxic_area_km2_matlab', width = 1, color = 'none', hatch='/////', edgecolor = 'black', linewidth = 0.5, label='matlab')
 
